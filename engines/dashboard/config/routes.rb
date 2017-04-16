3bee1af0822822d7771f6e8829a1e5078725a6cc
Nathaniel Miller <dukeoflaser@gmail.com>
2017-04-15 20:24:39 -0700

    Add devise module to route

--- engines/dashboard/config/routes.rb
+++ engines/dashboard/config/routes.rb
@@ -1,4 +1,4 @@
 Miller::Dashboard::Engine.routes.draw do
-  devise_for :users, class_name: "Miller::User"
+  devise_for :users, class_name: "Miller::User", module: :devise
   root to: "dashboard#index"
 end
