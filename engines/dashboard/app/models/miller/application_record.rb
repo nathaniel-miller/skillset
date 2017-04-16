c852a58d4bd9f65ac99b2b50bf91f2d1622c88eb
Nathaniel Miller <dukeoflaser@gmail.com>
2017-04-15 20:12:52 -0700

    Generate devise/rails user model

---
+++ engines/dashboard/app/models/miller/application_record.rb
@@ -0,0 +1,5 @@
+module Miller
+  class ApplicationRecord < ActiveRecord::Base
+    self.abstract_class = true
+  end
+end
--- engines/dashboard/config/routes.rb
+++ engines/dashboard/config/routes.rb
@@ -1,3 +1,4 @@
 Miller::Dashboard::Engine.routes.draw do
+  devise_for :users, class_name: "Miller::User"
   root to: "dashboard#index"
 end
